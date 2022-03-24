f9ce00c79e21a3450bfdff21e205a2f54dd294a5
thimoteo <thimoteo.oliveira@nre.tec.br>
2022-03-24 17:51:36 -0300

    deploy heroku4

--- cvideirasertao/settings.py
+++ cvideirasertao/settings.py
@@ -118,9 +118,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATICFILES_DIRS = [os.path.join(BASE_DIR, config("STATIC_ROOT", default="static"))]
-MEDIA_ROOT = config("MEDIA_ROOT", default="static")
-STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
