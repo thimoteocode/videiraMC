ac5f42b9b7a223374c05e3099ccbcddac852c2f0
thimoteo <thimoteo.oliveira@nre.tec.br>
2022-03-25 13:50:21 -0300

    deploy heroku7

--- cvideirasertao/urls.py
+++ cvideirasertao/urls.py
@@ -21,6 +21,4 @@ from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
