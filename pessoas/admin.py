32322c77dfbd4cf451e5b79f68a14b82b2ef3ecc
thimoteo <thimoteo.oliveira@nre.tec.br>
2022-03-28 14:00:04 -0300

    filter-custom

--- pessoas/admin.py
+++ pessoas/admin.py
@@ -1,7 +1,25 @@
 from django.contrib import admin
-
+from django.contrib.admin.filters import SimpleListFilter
 # Register your models here.
 from .models import Pessoa, FuncaoLideranca
+
+class CustomFilter(SimpleListFilter):
+    title = "Custom filter"
+    parameter_name = "custom"
+
+    def lookups(self, request, model_admin):
+        return {
+            ("palmeirinha", "Palmeirinha")
+            ("varzea dos bois", "Varzea dos bois")
+            ("bananeira", "Bananeira")
+            ("laranjeira", "Laranjeira")
+        }
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(povoado_text__contains=self.value())
+        return queryset        
+
 
 admin.site.register(Pessoa)
 admin.site.register(FuncaoLideranca)
---
+++ readme.md
@@ -0,0 +1,8 @@
+heroku run python manage.py createsuperuser
+
+heroku run python manage.py migrate
+
+heroku login
+
+git push -u heroku main
+
